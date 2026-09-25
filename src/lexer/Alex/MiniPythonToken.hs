2098c8198d46dc8be0bdde6f68fe6ed90c54ceee
AnaPaulaTadim <160544930+AnaPaulaTadim@users.noreply.github.com>
2026-09-25 17:00:55 -0300

    Add token and position data structures
    Define token structures and position data for the lexer.

--- src/lexer/Alex/MiniPythonToken.hs
+++ src/lexer/Alex/MiniPythonToken.hs
@@ -1,1 +1,28 @@
+--Lista de tokens do Alex(quais tokens existem)
+
+module MiniPythonToken where 
+
+--Guardando a posição de cada token 
+data Position = Position 
+{
+    line :: int,
+    colum :: int
+}
+deriving(Show, Eq)
+
+--Criando o token + posição + caractere lido
+data Token 
+    = Token Position Lexeme
+    deriving(Show, Eq)
+
+--caracters que vão sendo lidos(tipos básicos que o lexe produz)
+data Lexeme
+    = TkPlus
+    | TkEOf
+    deriving(Show, Eq)
+
+--Verifica se o token é um lex válido
+tokenLexeme :: Token -> Lexeme
+tokenLexeme (Token _ lx) =  lx
+
 
